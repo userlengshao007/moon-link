30afe7e0e4e2b5d54be583621173c428ecfc2283
wqnm1gb <820998522@qq.com>
2026-06-06 10:45:14 +0800

    opt: 客户端代码加上注释和不让关闭测试redis效果

--- moon-link-common/src/main/java/com/moon/link/common/constant/ChannelAttrKey.java
+++ moon-link-common/src/main/java/com/moon/link/common/constant/ChannelAttrKey.java
@@ -1,5 +1,8 @@
 package com.moon.link.common.constant;
 
 public class ChannelAttrKey {
+    // 断开连接的时候知道这个 channel 属于哪个用户
     public static final String USER_ID = "userId";
+    // 心跳累计几次后再续期 Redis
+    public static final String HEARTBEAT_TIMES = "heartBeatTimes";
 }
