b4ebe6b41bec28325978e2ef7d099044f26738df
wqnm1gb <820998522@qq.com>
2026-06-06 10:45:30 +0800

    opt: 心跳处理器会给redis续期

--- moon-link-core/src/main/java/com/moon/link/processor/HeartBeatProcessor.java
+++ moon-link-core/src/main/java/com/moon/link/processor/HeartBeatProcessor.java
@@ -1,9 +1,12 @@
 package com.moon.link.processor;
 
+import com.moon.link.common.constant.ChannelAttrKey;
 import com.moon.link.common.domain.protobuf.CompleteMessage;
 import com.moon.link.common.domain.protobuf.PacketBody;
 import com.moon.link.common.domain.protobuf.PacketHeader;
+import com.moon.link.redis.RedisClient;
 import io.netty.channel.ChannelHandlerContext;
+import io.netty.util.AttributeKey;
 
 import static com.moon.link.common.enums.MessageType.HEARTBEAT_MESSAGE;
 
@@ -11,6 +14,16 @@ public class HeartBeatProcessor extends AbstractMessageProcessor<CompleteMessage
     @Override
     public void process(ChannelHandlerContext ctx, CompleteMessage msg) {
         long uid = msg.getPacketHeader().getUid();
+
+        AttributeKey<Long> heartBeatTimesKey = AttributeKey.valueOf(ChannelAttrKey.HEARTBEAT_TIMES);
+        Long lastTimes = ctx.channel().attr(heartBeatTimesKey).get();
+        long heartBeatTimes = lastTimes == null ? 1 : lastTimes + 1;
+        ctx.channel().attr(heartBeatTimesKey).set(heartBeatTimes);
+
+        // 每三次心跳做一个续期
+        if (heartBeatTimes % 3 == 0) {
+            RedisClient.expireUserOnline(uid);
+        }
 
         CompleteMessage response = CompleteMessage.newBuilder()
                 .setPacketHeader(PacketHeader.newBuilder()
