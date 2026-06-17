a8bd6214f7d0e2e508553a0e208ba9d5b44bcacc
wqnm1gb <820998522@qq.com>
2026-06-17 18:59:40 +0800

    feature: 服务端空闲检测功能实现

--- moon-link-core/src/main/java/com/moon/link/config/NettyConfig.java
+++ moon-link-core/src/main/java/com/moon/link/config/NettyConfig.java
@@ -2,4 +2,13 @@ package com.moon.link.config;
 
 public class NettyConfig {
     public static final int NETTY_PORT = Integer.getInteger("moon.link.netty.port", 9999);
+
+    /**
+     * 服务端读空闲超时时间。
+     * <p>
+     * 客户端正常情况下应该定时发送心跳消息；如果服务端超过这个时间没有读到任何客户端数据，
+     * 就认为连接可能已经假死，后续由 IdleStateHandler 触发关闭。
+     */
+    public static final int READER_IDLE_SECONDS =
+            Integer.getInteger("moon.link.netty.reader-idle-seconds", 90);
 }
--- moon-link-core/src/main/java/com/moon/link/netty/NettyServer.java
+++ moon-link-core/src/main/java/com/moon/link/netty/NettyServer.java
@@ -2,16 +2,19 @@ package com.moon.link.netty;
 
 import com.moon.link.codec.MessageProtocolDecoder;
 import com.moon.link.codec.MessageProtocolEncoder;
-import com.moon.link.common.domain.protobuf.CompleteMessage;
+import com.moon.link.config.NettyConfig;
 import com.moon.link.handler.LinkChannelHandler;
+import com.moon.link.handler.ServerIdleStateHandler;
 import com.moon.link.link.LinkConfig;
-import com.moon.link.redis.RedisClient;
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class NettyServer {
@@ -46,8 +49,19 @@ public class NettyServer {
                         @Override
                         protected void initChannel(SocketChannel ch) {
                             ch.pipeline()
+                                    // 读空闲检测：超过配置时间没有收到客户端任何消息，就触发 READER_IDLE 事件。
+                                    // 客户端正常会定时发心跳，所以只有断网、假死、长时间不发消息时才会触发。
+                                    .addLast(new IdleStateHandler(
+                                            NettyConfig.READER_IDLE_SECONDS,
+                                            0,
+                                            0,
+                                            TimeUnit.SECONDS
+                                    ))
                                     .addLast(new MessageProtocolDecoder())
                                     .addLast(new MessageProtocolEncoder())
+                                    // 处理 IdleStateHandler 触发的空闲事件，只负责关闭 channel。
+                                    // 真正的 UserChannelCtxMap 和 Redis 清理由 LinkChannelHandler.channelInactive 统一完成。
+                                    .addLast(new ServerIdleStateHandler())
                                     .addLast(new LinkChannelHandler());
                         }
                     });
